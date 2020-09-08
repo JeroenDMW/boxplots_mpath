8218fb57b79694551cab7a11eac1fa207201bc5e
Jeroen Weermeijer <u0100835@kuleuven.be>
2020-09-08 13:28:28 +0200

    added code

--- boxplot_script.R
+++ boxplot_script.R
@@ -1,12 +1,7 @@
 require(xlsx)
 df = read.xlsx("tester iOS beta.xlsx", sheetName = 1, colNames=TRUE)
 
-df %>%
-  ggplot(aes(x=carrier, y=speed)) +
-  geom_boxplot() +
-  labs(y="Speed", x="Carrier", subtitle="Speed vs Carrier: nycflight13 data")
 
 boxplot(df, use.cols=T)
 
-
-
+#hello Lena
